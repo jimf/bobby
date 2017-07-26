ba4fc840c8652ac2e24af9c2f41b7e37007df725
Jim Fitzpatrick <fitzpatrick.jim@gmail.com>
2017-07-25 20:31:07 -0400

    Render board using getBoardSquare util

--- src/views/board.js
+++ src/views/board.js
@@ -1,29 +1,22 @@
 var html = require('choo/html')
 var square = require('./square')
+var _ = require('../util')
 
-function range (limit) {
-  var result = []
-  var i = 0
-  while (i < limit) {
-    result.push(i)
-    i += 1
-  }
-  return result
-}
+var positions = [8, 7, 6, 5, 4, 3, 2, 1].reduce(function (acc, rank) {
+  return acc.concat('abcdefgh'.split('').map(function (file) {
+    return { rank: rank, file: file }
+  }))
+}, [])
 
 module.exports = function (props) {
-  var squares = props.game.getStatus().board.squares
-
-  function renderSquare (index) {
-    var x = index % 8
-    var y = 7 - Math.floor(index / 8)
-    return square(squares[8 * y + x])
+  function renderSquare (pos) {
+    return square(_.getBoardSquare(pos.file, pos.rank, props.game))
   }
 
   return html`
     <div class="board">
       <div class="board__inner">
-        ${range(64).map(renderSquare)}
+        ${positions.map(renderSquare)}
       </div>
     </div>
   `
