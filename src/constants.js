1d966b327c1418036ae25d1ce316883fdb39bdb1
Jim Fitzpatrick <fitzpatrick.jim@gmail.com>
2017-08-15 13:14:07 -0400

    Add blue and mahogany themes

--- src/constants.js
+++ src/constants.js
@@ -29,5 +29,21 @@ exports.themes = [
     blackSquare: '#6a9058',
     active: '#f7f68d',
     recent: '#b8ca55'
+  },
+  {
+    value: 'blue-silver',
+    label: 'Blue and Silver',
+    whiteSquare: '#efefef',
+    blackSquare: '#6f85ba',
+    active: '#ffec8c',
+    recent: '#ffc18c'
+  },
+  {
+    value: 'mahogany',
+    label: 'Mahogany',
+    whiteSquare: '#F8D9A9',
+    blackSquare: '#D07238',
+    active: '#f7f68d',
+    recent: '#ffbb90'
   }
 ]
