210142736070bdd21cfa56568657d30bb850c79b
Jim Fitzpatrick <fitzpatrick.jim@gmail.com>
2017-08-18 12:19:31 -0400

    Update board pieces to use svg

--- src/views/chess_piece.js
+++ src/views/chess_piece.js
@@ -1,10 +1,9 @@
 var html = require('choo/html')
-var c = require('../constants')
 
 module.exports = function (props) {
   return html`
-    <span class="board__piece board__piece--is-${props.side.name}">
-      ${c.symbols[props.side.name + props.type]}
-    </span>
+    <svg class="board__piece board__piece--is-${props.side.name}">
+      <use xlink:href="#${props.side.name}-${props.type}" />
+    </svg>
   `
 }
