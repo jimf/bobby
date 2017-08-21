3e4649f661208126308a1584fe7cce91566d1cf6
Jim Fitzpatrick <fitzpatrick.jim@gmail.com>
2017-08-21 12:27:42 -0400

    Fix rendering for square/tall viewports

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 var choo = require('choo')
+var debounce = require('lodash.debounce')
 
 var app = choo()
 var prefs = window.localStorage.getItem('bobby')
@@ -12,3 +13,21 @@ app.use(require('./model')({
   }
 }))
 app.mount('#app')
+
+function getWindowAspectRatio () {
+  var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0)
+  var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0)
+  return w / h
+}
+
+function updateIsTall () {
+  var aspectRatio = getWindowAspectRatio()
+  if (aspectRatio < 1.34) {
+    document.body.classList.add('is-tall')
+  } else {
+    document.body.classList.remove('is-tall')
+  }
+}
+
+updateIsTall()
+window.addEventListener('resize', debounce(updateIsTall))
